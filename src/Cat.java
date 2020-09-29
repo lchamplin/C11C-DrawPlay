f1acff901c9e4c7aca120bcb7acbe9a3c74089ce
Lilly Raeanne Sline <lillyraeannesline@csm-wl-dhcp-195-82.mines.edu>
2020-09-29 11:55:40 -0600

    changed g2.setColor to blue

--- src/Cat.java
+++ src/Cat.java
@@ -42,7 +42,7 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.red);
+		g2.setColor(Color.blue);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
